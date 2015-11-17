55cf0089557e25c0b0e0fa95f96205dc77d0460d
Michael P. Geraci <me@mgeraci.com>
2015-11-16 21:46:37 -0500

    set up a basic flask app to make a single article search

---
+++ nytimes-music-rss.py
@@ -0,0 +1,29 @@
+from flask import Flask
+from flask import render_template
+from flask import jsonify
+from flask import Response
+import requests
+
+# constants
+from constants import C
+
+# secret settings
+from localsettings import ARTICLES_API_KEY
+from localsettings import EVENTS_API_KEY
+
+app = Flask(__name__)
+
+@app.route('/')
+def index_route(params={}):
+    q = 'ben+ratliff'
+    url = '{root}?q={query}&api-key={key}'.format(
+        root = C['API_ROOT'],
+        query = q,
+        key = ARTICLES_API_KEY
+    )
+
+    r = requests.get(url)
+    return Response(r.text, mimetype='text/json')
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
