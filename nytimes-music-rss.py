afd56f7ac7a863d8788dbfbd2100d35b7f0c4514
Michael P. Geraci <me@mgeraci.com>
2015-11-17 21:02:40 -0500

    make a variable number of queries and return an array of results by query; update gitignore

--- nytimes-music-rss.py
+++ nytimes-music-rss.py
@@ -14,20 +14,38 @@ from localsettings import EVENTS_API_KEY
 
 app = Flask(__name__)
 
+
+# routes
+###############################################################################
+
 @app.route('/')
 def index_route(params={}):
+    queries = ['ben+ratliff', 'jon+pareles']
+
+    # make an array of the size of the number of queries, defaulting to None
+    results = [None for i in range(len(queries))]
+
+    for i, query in enumerate(queries):
+        url = generate_url(query)
+        res = json.loads(requests.get(url).text)
+        res = format_response(res['response']['docs'])
+        results[i] = res
+
+    return str(results)
+
+
+# helpers
+###############################################################################
+
+def generate_url(query):
     sort = 'newest'
-    q = 'ben+ratliff'
-    url = '{root}?q={query}&api-key={key}&sort={sort}'.format(
+
+    return '{root}?q={query}&api-key={key}&sort={sort}'.format(
         root = C['API_ROOT'],
-        query = q,
+        query = query,
         sort = sort,
         key = ARTICLES_API_KEY,
     )
-
-    r = json.loads(requests.get(url).text)
-    r = format_response(r['response']['docs'])
-    return str(r)
 
 def format_response(articles):
     res = []
@@ -36,9 +54,16 @@ def format_response(articles):
         o = {}
         o['title'] = article['headline']['main']
 
+        if article['byline'] != None:
+            o['author'] = article['byline']['original']
+
         res.append(o)
 
     return res
+
+
+# init
+###############################################################################
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
